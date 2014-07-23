81a875ad5d9c0a5ddaeb292103ee9f37f41fee31
Derek Liu <liuxianyang94@hotmail.com>
2014-07-23 14:27:51 -0700

    Version 1
    GUI

---
+++ UploadAPIDemoGUI/Upload.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadAPIDemo
+{
+    /// <summary>
+    /// Object holding information returned by REST API for upload
+    /// </summary>
+    class Upload : BaseObject
+    {
+        public Upload() { }
+
+        public Upload(string deliveryID, string sessionName)
+        {
+            SessionID = deliveryID;
+            UploadTarget = sessionName;
+        }
+
+        public string SessionID { get; set; }
+
+        public string UploadTarget { get; set; }
+    }
+}
